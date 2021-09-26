97851b037dc410397a6f9886c122fda4f48772b4
sharmivijay <89096043+sharmivijay@users.noreply.github.com>
2021-09-26 16:18:39 +0530

    feat - Add emoji interpretation and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var fruitsDictionary = {
+    "🍊": "Tangerine",
+    "🍋": "Lemon",
+    "🍍": "Pineapple",
+    "🥭": "Mango",
+    "🍎": "Red Apple",
+    "🍏": "Green Apple",
+    "🍐": "Pear",
+    "🍑": "Peach"
+  };
+
+  var [fruitName, setFruitName] = useState("");
+  const keyList = Object.keys(fruitsDictionary);
+
+  function callOnChangeEvent(event) {
+    var userInput = event.target.value;
+    var outputKey = "";
+
+    if (userInput in fruitsDictionary) {
+      outputKey = fruitsDictionary[userInput];
+    } else {
+      outputKey = Object.keys(fruitsDictionary).find(
+        (key) => fruitsDictionary[key] === userInput
+      );
+    }
+    if (userInput === "") {
+      outputKey = "";
+    } else {
+      if (outputKey === undefined) {
+        outputKey = "Not in database";
+      }
+    }
+    setFruitName(outputKey);
+  }
+
+  function emojiHandler(emoji) {
+    setFruitName(fruitsDictionary[emoji]);
+  }
+
   return (
     <div className="App">
-      <h1>Hello hi</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>I like Fruits .. Do you ?</h1>
+      <input onChange={callOnChangeEvent} className="input" />
+      <div className="output"> {fruitName}</div>
+      <div>
+        {keyList.map((key) => {
+          return (
+            <li>
+              <button className="btn-emoji" onClick={() => emojiHandler(key)}>
+                {key}
+              </button>
+            </li>
+          );
+        })}
+      </div>
     </div>
   );
 }
